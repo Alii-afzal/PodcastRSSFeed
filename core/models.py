52ab58b84ca8da4243f259a5cad2288138418d2e
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-09-27 14:25:57 +0330

    feat:replace modelsd.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Channel(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True, null=True)
+    subtitle = models.CharField(max_length=30, blank=True, null=True)
+    language = models.CharField(max_length=30, blank=True, null=True)
+    pubDate = models.DateTimeField(blank=True, null=True)
+    duration = models.CharField(max_length=50 , blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.URLField(blank=True, null=True)
+    owner = models.BooleanField(null=True)
+    author = models.TextField()
+    source = models.URLField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+    
+
+class Episode(models.Model):
+    title = models.CharField(max_length=30)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    subtitle = models.CharField(max_length=30, blank=True, null=True)
+    guid = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    pub_date = models.DateTimeField(blank=True, null=True)
+    duration = models.CharField(max_length=50, blank=True, null=True)
+    audio_file = models.URLField()
+    image = models.URLField(blank=True, null=True)
+    explicit = models.BooleanField(blank=True, null=True)
+
+    
+    def __str__(self):
+        return self.title
+    
+class News(models.Model):
+    title = models.CharField(max_length=50)
+    channel =models.ForeignKey(Channel, on_delete=models.CASCADE)
+    guid = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(blank=True, null=True)
+    image = models.URLField(blank=True, null=True)
+    source = models.URLField(blank=True, null=True)
+    
+    
+    def __str__(self):
+        return self.title
+    
