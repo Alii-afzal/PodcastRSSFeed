2b42512fd539c0d2bd674c2da34ec975873b13d1
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-09-28 14:59:50 +0330

    fix:fix some problems

--- core/views.py
+++ core/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 # from RSSfeed.parsers import PodcastParser, Parser, xml_data
 from rest_framework.views import APIView
-from rssfeedparser.serializers import ChannelSerializer, EpisodeSerializer
+from .serializers import ChannelSerializer, EpisodeSerializer
 from rest_framework.response import Response
-from rssfeedparser.models import Episode, Channel
+from .models import Episode, Channel
 from .parser import channel_parser, item_parser
 class ExtractXML(APIView):
     def get(self, request):
-        channel_parser()
+        # channel_parser()
         parsed_data = Channel.objects.all()
         ser_data = EpisodeSerializer(instance=parsed_data, many=True)
         return Response(data=ser_data.data)
