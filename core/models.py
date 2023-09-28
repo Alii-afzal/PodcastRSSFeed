06b2b148b2806e52ad17703b0f856a64a0c6123a
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-09-28 14:57:14 +0330

    feat:add Category model and fix som fields

--- core/models.py
+++ core/models.py
@@ -4,7 +4,11 @@ class Type(models.Model):
     pass
 
 class Category(models.Model):
-    pass
+    title = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.title
+    
 
 class XmlLink(models.Model):
     xml_link = models.URLField(unique=True)
@@ -17,7 +21,7 @@ class Channel(models.Model):
     language = models.CharField(max_length=30, blank=True, null=True)
     pubDate = models.DateTimeField(blank=True, null=True)
     duration = models.CharField(max_length=50 , blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     image = models.URLField(blank=True, null=True)
     owner = models.BooleanField(null=True)
     author = models.TextField()
@@ -29,11 +33,11 @@ class Channel(models.Model):
 
 class Episode(models.Model):
     title = models.CharField(max_length=30)
-    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, null=True, blank=True)
     subtitle = models.CharField(max_length=30, blank=True, null=True)
     guid = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    pub_date = models.DateTimeField(blank=True, null=True)
+    pubDate = models.DateTimeField(blank=True, null=True)
     duration = models.CharField(max_length=50, blank=True, null=True)
     audio_file = models.URLField()
     image = models.URLField(blank=True, null=True)
@@ -47,7 +51,7 @@ class News(models.Model):
     title = models.CharField(max_length=50)
     channel =models.ForeignKey(Channel, on_delete=models.CASCADE)
     guid = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(blank=True, null=True)
+    pubDate = models.DateTimeField(blank=True, null=True)
     image = models.URLField(blank=True, null=True)
     source = models.URLField(blank=True, null=True)
     
