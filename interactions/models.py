b1a607eda38595a48378fbc4ac3054b21440b987
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-09-29 11:05:42 +0330

    feat:create Subscribe model

--- interactions/models.py
+++ interactions/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from core.models import Channel, Episode
+from accounts.models import User
 
 # Create your models here.
+class Subscribe(models.Model):
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    user =  models.ForeignKey(User, on_delete=models.CASCADE)
+    
