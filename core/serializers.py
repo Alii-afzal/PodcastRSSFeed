5791df749bc0a3f947883dec018f40e7035c6cfc
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-09-29 10:36:08 +0330

    fix:fix serializaer errror

--- core/serializers.py
+++ core/serializers.py
@@ -4,33 +4,35 @@ from .models import Channel, Episode, News,  XmlLink
 class ChannelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Channel
-        fields = [
-            'title',
-            'subtitle',
-            'description',
-            'pubDate',
-            'image',
-            'language',
-            'author',
-            'source',
-            'owner',
-        ]
+        fields="__all__"
+        # fields = [
+        #     'title',
+        #     'subtitle',
+        #     'description',
+        #     'pubDate',
+        #     'image',
+        #     'language',
+        #     'author',
+        #     'source',
+        #     'owner',
+        # ]
     
         
 class EpisodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Episode
-        fields = [
-            'title',
-            'subtitle',
-            'description',
-            'guid',
-            'pub_date',
-            'duration',
-            'audio_file',
-            'image',
-            'explicit',
-        ]
+        fields = "__all__"
+        # fields = [
+        #     'title',
+        #     'subtitle',
+        #     'description',
+        #     'guid',
+        #     'pubDate',
+        #     'duration',
+        #     'audio_file',
+        #     'image',
+        #     'explicit',
+        # ]
         
         # depth = 2
     
