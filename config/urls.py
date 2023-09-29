0cdd98f2b3aad7c89eb1069f73142166eb1e6964
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-09-29 10:13:30 +0330

    feat:add interactions endpoint

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('parser/', include('core.urls')),
+    path('interactions/', include('interactions.urls')),
 ]
