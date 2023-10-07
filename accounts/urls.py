b9fcada3a583bbdfec1d595a4d989259413883e7
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-07 12:03:58 +0330

    fix: import new view classes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from accounts.views import RegisterAPIView, LoginAPIView, UserAPIView, RefreshAPIView, LogoutAPIView
-
+from accounts.views import RegisterAPIView, LoginAPIView
+# , UserAPIView, RefreshAPIView, LogoutAPIView
 
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
-    path('user/', UserAPIView.as_view(), name='user'),
-    path('refresh/', RefreshAPIView.as_view(), name='refresh'),
-    path('logout/', LogoutAPIView.as_view(), name='logout'),
+    # path('user/', UserAPIView.as_view(), name='user'),
+    # path('refresh/', RefreshAPIView.as_view(), name='refresh'),
+    # path('logout/', LogoutAPIView.as_view(), name='logout'),
     
 ]
 
