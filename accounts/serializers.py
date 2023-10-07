9b1b2bac913e2e63c0150617f91ec1a79d140012
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-07 12:03:18 +0330

    fix :fix UserRegisterSerializer and remove create function

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,25 +1,33 @@
 from rest_framework import serializers
 from .models import User
 
-class UserSerializer(serializers.ModelSerializer):
+# def clean_email(value):
+#     if 'admin' in value:
+#         raise serializers.ValidationError('admin can`t be in email')
 
+class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", 'phone_number', 'email', 'password']
+        fields = ["id", "phone_number", "email", "password"]
+        extra_kwargs = {
+            'password': {'write_only': True},
+            # 'email' : {'valiators': (clean_email, )}
+        }
+    
+    def validate_username(self, value):
+        if value == 'admin':
+            raise serializers.ValidationError('username cant be `admin`')
+        return value
+        
+class UserLoginSerializer(serializers.Serializer):
+    class Meta:
+        model:User
+        fields = ["id", "email", "password"]
         extra_kwargs = {
             'password': {'write_only': True}
         }
-
-    def create(self, validated_data):
-        password = validated_data.pop('password', None) # extracting password form the inputs
-        instance = self.Meta.model(**validated_data) # creating the user
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
+    
+    
+    
     
 
-# class UserRegisterSerializer(serializers.Serializer):
-#     phone_number =serializers.CharField(required=True)
-#     email = serializers.EmailField(required=True)
-#     password = serializers.CharField(required=True)
