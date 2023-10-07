c3eb9f4c40593c8479520c4af7803db583ecaab1
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-07 12:21:13 +0330

    feat:add path like

--- interactions/urls.py
+++ interactions/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import like_episode
+# from .views import like_episode
+from .views import LikeAPIView
 
 urlpatterns = [
-    path('like/<int:episode_id>/', like_episode, name='like_episode'),
+    path('like/', LikeAPIView.as_view())
+    # path('like/<int:episode_id>/', like_episode, name='like_episode'),
     # path('', SubscribeApiView.as_view()),
     # path('', CommentApiView.as_view()),
 ]
