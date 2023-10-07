d485c896d427647a6a59ef09d269cbc5b0e1bd31
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-07 12:21:53 +0330

    feat:register all interactions models

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Like, Comment, Subscribe
 
 # Register your models here.
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Subscribe)
