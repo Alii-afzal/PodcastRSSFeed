ef35212fc5053b575780d964a4c5ca12bc27629c
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-07 12:15:22 +0330

    feat:register all models

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Channel, Episode, News
+from .models import Channel, Episode, News, Category, XmlLink, News, Type
 # Register your models here.
 admin.site.register(Channel)
 admin.site.register(Episode)
+admin.site.register(Category)
+admin.site.register(XmlLink)
+admin.site.register(News)
+admin.site.register(Type)
