68c395960ab9f986e93366bf911c99b20f2fdeed
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-08 17:49:27 +0330

    fix:delete some comments

--- core/management/commands/parse.py
+++ core/management/commands/parse.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
-from core.parser import channel_parser, item_parser
-from core.parsers import Parser, PodcastParser
+from core.parser import XMLParser
 import requests
 
 class Command(BaseCommand):
@@ -8,18 +7,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            # podcast_parse_task.delay()
-            
-            # channel_parser()
-            item_parser()
-            # response = requests.get('https://rss.art19.com/apology-line')
-            # xml_data = response.text 
-            # parser = Parser(xml_data)
-            # parser.parse_channel()
-            # podcastparser = PodcastParser(xml_data)
-            # podcastparser.parse_xmls()
-            
-                
+            XMLParser.channel_parser()
+            XMLParser.item_parser()    
             self.stdout.write(
                     self.style.SUCCESS(
                         f"Successfully parsed and updated podcast data for all models)"
