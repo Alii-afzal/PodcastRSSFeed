92a5529a0c4d329e0bee1795acf5cbad9774f8e9
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-09 22:20:00 +0330

    feat:add path login,refresh,logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from accounts.views import RegisterAPIView, LoginAPIView
+from accounts.views import RegisterAPIView, LoginAPIView, AuthenticatedView, RefreshAPIView, LogoutAPIView
 # , UserAPIView, RefreshAPIView, LogoutAPIView
 
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
-    # path('user/', UserAPIView.as_view(), name='user'),
-    # path('refresh/', RefreshAPIView.as_view(), name='refresh'),
-    # path('logout/', LogoutAPIView.as_view(), name='logout'),
+    path('jwtauthenticate/', AuthenticatedView.as_view()),
+    path('refresh/', RefreshAPIView.as_view(), name='refresh'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
 ]
