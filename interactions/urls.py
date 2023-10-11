31b6b611a49599a46fad22e970bbffc310cbe284
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-11 12:42:18 +0330

    fix:delete some comments

--- interactions/urls.py
+++ interactions/urls.py
@@ -4,7 +4,4 @@ from .views import LikeAPIView
 
 urlpatterns = [
     path('like/', LikeAPIView.as_view())
-    # path('like/<int:episode_id>/', like_episode, name='like_episode'),
-    # path('', SubscribeApiView.as_view()),
-    # path('', CommentApiView.as_view()),
 ]
