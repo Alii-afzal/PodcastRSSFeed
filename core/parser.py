4f97d2131515205e2703ff12f078a841f543aa32
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-11 12:36:18 +0330

    feat:add __init__ function to XMLParser class

--- core/parser.py
+++ core/parser.py
@@ -4,19 +4,20 @@ import xml.etree.ElementTree as ET
 from .models import Channel, Episode, Category
 
 class XMLParser():
-    response = requests.get('https://rss.art19.com/apology-line')
-    xml_data = response.text
-    root = ET.fromstring(xml_data)
-    itunes_namespace = {'itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd'}
-        
-    def convert_text_to_datefield(date_str):
+    def __init__(self):
+        self.response = requests.get('https://rss.art19.com/apology-line')
+        self.xml_data = self.response.text
+        self.root = ET.fromstring(self.xml_data)
+        self.itunes_namespace = {'itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd'}
+            
+    def convert_text_to_datefield(self, date_str):
         custom_format='%a, %d %b %Y %H:%M:%S %z'
         if date_str:
             return datetime.strptime(date_str, custom_format)
         else:
             return None
 
-    def convert_text_to_boolianfield(explicit_str):
+    def convert_text_to_boolianfield(self, explicit_str):
         if explicit_str == 'yes':
             return True
         elif explicit_str == 'no':
@@ -24,22 +25,21 @@ class XMLParser():
         else:
             return None
 
-    def XML_link_parser():
-        pass
+    # def XML_link_parser():
+    #     pass
 
-    def channel_parser():
-        
-        for elm in root.findall(".//channel", itunes_namespace):
+    def channel_parser(self):
+        for elm in self.root.findall(".//channel", self.itunes_namespace):
             title = elm.find('.//title').text if elm.find('.//title') is not None else None
             subtitle = elm.find('.//subtitle').text if elm.find('.//subtitle') is not None else None
             description = elm.find('.//description').text if elm.find('.//description') is not None else None
-            pubDate = convert_text_to_datefield(elm.find('.//pubDate').text) if elm.find('.//pubDate') is not None else None
-            image = elm.find('.//itunes:image', itunes_namespace).attrib.get('href') if elm.find('.//itunes:image', itunes_namespace) is not None else None
+            pubDate = self.convert_text_to_datefield(elm.find('.//pubDate').text) if elm.find('.//pubDate') is not None else None
+            image = elm.find('.//itunes:image', self.itunes_namespace).attrib.get('href') if elm.find('.//itunes:image', self.itunes_namespace) is not None else None
             language = elm.find('.//language').text if elm.find('.//language') is not None else None
-            author = elm.find('.//itunes:author', itunes_namespace).text if elm.find('.//itunes:author', itunes_namespace) is not None else None
-            category = elm.find('.//itunes:category', itunes_namespace).get('text') if elm.find('.//itunes:category', itunes_namespace) is not None else None
+            author = elm.find('.//itunes:author', self.itunes_namespace).text if elm.find('.//itunes:author', self.itunes_namespace) is not None else None
+            category = elm.find('.//itunes:category', self.itunes_namespace).get('text') if elm.find('.//itunes:category', self.itunes_namespace) is not None else None
             source =elm.find('.//link').text if elm.find('.//link') is not None else None
-            owner = convert_text_to_boolianfield(elm.find('.//itunes:owner/itunes:name', itunes_namespace)) if elm.find('.//itunes:owner/itunes:name', itunes_namespace) is not None else None
+            owner = self.convert_text_to_boolianfield(elm.find('.//itunes:owner/itunes:name', self.itunes_namespace)) if elm.find('.//itunes:owner/itunes:name', self.itunes_namespace) is not None else None
 
         category = Category(
             title=category
@@ -61,22 +61,18 @@ class XMLParser():
 
         channel.save()
         
-    def item_parser():
-        response = requests.get('https://rss.art19.com/apology-line')
-        xml_data = response.text
-        root = ET.fromstring(xml_data)
-        itunes_namespace = {'itunes':'http://www.itunes.com/dtds/podcast-1.0.dt'}
-        all_episodes = []  # Create a list to store all Episode dictionaries
-        for elm in root.findall(".//item", itunes_namespace):
+    def item_parser(self):
+        all_episodes = []
+        for elm in self.root.findall(".//item", self.itunes_namespace):
             title = elm.find('.//title').text if elm.find('.//title') is not None else None
             subtitle = elm.find('.//subtitle').text if elm.find('.//subtitle') is not None else None
             description = elm.find('.//description').text if elm.find('.//description') is not None else None
-            pubDate = convert_text_to_datefield(elm.find('.//pubDate').text) if elm.find('.//pubDate') is not None else None
-            image = elm.find('.//itunes:image', itunes_namespace).attrib.get('href') if elm.find('.//itunes:image', itunes_namespace) is not None else None
-            duration = elm.find('.//itunes:duration', itunes_namespace).text if elm.find('.//itunes:duration', itunes_namespace) is not None else None
+            pubDate = self.convert_text_to_datefield(elm.find('.//pubDate').text) if elm.find('.//pubDate') is not None else None
+            image = elm.find('.//itunes:image', self.itunes_namespace).attrib.get('href') if elm.find('.//itunes:image', self.itunes_namespace) is not None else None
+            duration = elm.find('.//itunes:duration', self.itunes_namespace).text if elm.find('.//itunes:duration', self.itunes_namespace) is not None else None
             audio_file = elm.find('.//enclosure').attrib.get('url') if elm.find('.//enclosure') is not None else None
             guid = elm.find('.//guid').text if elm.find('.//guid') is not None else None
-            explicit = convert_text_to_boolianfield(elm.find('.//itunes:explicit', itunes_namespace).text) if elm.find('.//itunes:explicit', itunes_namespace) is not None else None
+            explicit = self.convert_text_to_boolianfield(elm.find('.//itunes:explicit', self.itunes_namespace).text) if elm.find('.//itunes:explicit', self.itunes_namespace) is not None else None
 
             episode_data = {
                 'title': title,
@@ -103,3 +99,4 @@ class XMLParser():
     #         if not Episode.objects.filter(guid=item['guid']).exists():
     #             new_items.append(item)
     #     # for 
+    
