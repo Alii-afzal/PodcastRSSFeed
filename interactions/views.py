ec6a7c1790809ae4a1b9ab5bd32e18e674ed3f73
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-11 12:51:57 +0330

    feat:add SubscribeAPIView class

--- interactions/views.py
+++ interactions/views.py
@@ -48,3 +48,16 @@ class CommentAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class SubscribeAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+    serializer_class = SubscribeSerializer
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
