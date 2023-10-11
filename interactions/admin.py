b939d1f86e152296f970e60df54283af58677a82
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-11 12:38:12 +0330

    feat:register BookMark model

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Like, Comment, Subscribe
+from .models import Like, Comment, Subscribe, BookMark
 
 # Register your models here.
 admin.site.register(Like)
 admin.site.register(Comment)
 admin.site.register(Subscribe)
+admin.site.register(BookMark)
