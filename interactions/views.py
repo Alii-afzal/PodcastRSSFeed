3d5f6bf61dc42dacb9bb73a4f2a1076fd73874c0
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-12 10:00:22 +0330

    fix:fix some problems in interaction classes

--- interactions/views.py
+++ interactions/views.py
@@ -14,21 +14,21 @@ from accounts.authentication import JWTAuthentication
 class LikeAPIView(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = LikeSerializer
+    authentication_classes = [JWTAuthentication]
     
-    def post(self, request, episode_id):
-        episode = Episode.objects.get(id=episode_id)
+    def post(self, request):
+        episode = Episode.objects.get(id=request.data.get('episode_id'))
         user = request.user
-        
         like, created = Like.objects.get_or_create(user=user, episode=episode)
-        if existing_like:
+        if created:
             serializer = self.serializer_class(like)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             msg = {'status':'This episode is already liked!'}
             return Response(msg, status=status.HTTP_200_OK)
     
-    def delete(self, request, episode_id):
-        episode = PodcastEpisodeData.objects.get(id=episode_id)
+    def delete(self, request):
+        episode = Episode.objects.get(id=request.data.get('episode_id'))
         try:
             like = Like.objects.get(user=request.user, episode=episode)
             like.delete()
@@ -42,12 +42,23 @@ class CommentAPIView(APIView):
     permission_classes = (IsAuthenticated,)
     authentication_classes = (JWTAuthentication,)
     serializer_class = CommentSerializer
-    def post(self, request, episoe_id):
-        episode = Episode.objects.get(id=episoe_id)
+    
+    def post(self, request):
+        episode = Episode.objects.get(id=request.data.get('episode_id'))
+        
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user, episode=episode)
         
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    # def post(self, request):
+    #     episode = Episode.objects.get(id=request.data.get('episode_id'))
+        
+    #     serializer = self.serializer_class(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+        
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class SubscribeAPIView(APIView):
@@ -55,20 +66,25 @@ class SubscribeAPIView(APIView):
     authentication_classes = (JWTAuthentication,)
     serializer_class = SubscribeSerializer
     
+    x = []
+    print(x)
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid() and serializer.validated_data.get('user') not in self.x:
+            
+            self.x.append(serializer.validated_data.get('user'))
+            
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-class BookMarkSrializer(APIView):
+class BookMarkAPIView(APIView):
     permission_classes = (IsAuthenticated,)
     authentication_classes = (JWTAuthentication,)
     serializer_class = BookMarkSerializer
     
-    def post(self, request, episod_id):
-        episode = Episode.objects.get(id=episod_id)
+    def post(self, request):
+        episode = Episode.objects.get(id=request.data.get('episode_id'))
         bookmark_episode, created = BookMark.objects.get_or_create(user=request.user, episode=episode)
         
         if created:
@@ -77,10 +93,10 @@ class BookMarkSrializer(APIView):
             return Response(msg, status=status.HTTP_201_CREATED)
         else:
             msg = {'status':'This episode is already bookmarked'}
-            return Response(mag, status=status.HTTP_200_OK)
+            return Response(msg, status=status.HTTP_200_OK)
         
-    def delete(self, request, episode_id):
-        episode = PodcastEpisodeData.objects.get(id=episode_id)
+    def delete(self, request):
+        episode = Episode.objects.get(id=request.data.get('episode_id'))
         try:
             bookmarked_episode = BookMark.objects.get(user=request.user, episode=episode)
             bookmarked_episode.delete()
