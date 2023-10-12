f1cac64a59b827381195184849facc553f374664
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-12 10:03:56 +0330

    feat:add update_all_podcast function

--- core/tasks.py
+++ core/tasks.py
@@ -1,17 +1,12 @@
-# from celery import shared_task
-# from .parser import XMLParser
-# from .models import Channel, Episode, XmlLink
+from celery import shared_task
+from .parser import XMLParser
+from .models import Channel, Episode, XmlLink
 # from celery.utils.log import get_task_logger
 
-# logger= get_task_logger(__name__)
 
-# @shared_task(bind=True)
-# def update_podcast(self):
-#     xml_urls=XmlLink.objects.all()
-#     try:
-#         for xml_url in xml_urls:
-#             pass
-        
-#     except:
-#         logger.info()
-#         self.retry()
+
+@shared_task
+def update_all_podcast():
+    url_list = Podcast.objects.all().values_list('podcast_url')
+    for url in url_list:
+        update_podcast.delay(url)
