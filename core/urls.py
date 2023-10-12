0219ef3020c99d68221ed6dda4e9e17692cd3c5a
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-12 15:37:31 +0330

    feat:add update_episode endpoint

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ExtractXMLChannel, ExtractXMLItems
+from .views import ExtractXMLChannel, ExtractXMLItems, UpdateEpisodesView
  
  
 urlpatterns = [
     path('extractchannel/', ExtractXMLChannel.as_view()),
     path('extractitem/', ExtractXMLItems.as_view()),
+    path('update_episode/', UpdateEpisodesView.as_view())
 ]
