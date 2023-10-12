af614da8dab61accfd7b14a3906f7fccf09db223
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-12 10:05:11 +0330

    feat:add  comment,subscribe,bookmark ecndpoint

--- interactions/urls.py
+++ interactions/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 # from .views import like_episode
-from .views import LikeAPIView
+from .views import LikeAPIView, CommentAPIView, SubscribeAPIView, BookMarkAPIView
 
 urlpatterns = [
-    path('like/', LikeAPIView.as_view())
+    path('like/', LikeAPIView.as_view()),
+    path('comment/', CommentAPIView.as_view()),
+    path('subscribe/', SubscribeAPIView.as_view()),
+    path('bookmark/', BookMarkAPIView.as_view()),
 ]
