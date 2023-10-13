039abae4ed371848d4636e1fa1a623b67dd803f2
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-13 17:07:20 +0330

    feat:add path test

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from .views import ExtractXMLChannel, ExtractXMLItems, UpdateEpisodesView
 urlpatterns = [
     path('extractchannel/', ExtractXMLChannel.as_view()),
     path('extractitem/', ExtractXMLItems.as_view()),
-    path('update_episode/', UpdateEpisodesView.as_view())
+    path('update_episode/', UpdateEpisodesView.as_view()),
+    # path('test/', TestView.as_view()),
 ]
