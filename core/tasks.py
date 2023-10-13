64766b1230f5960f7cc88f04376f6fd2d64c1da9
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-13 17:06:53 +0330

    fix:comment all codes

--- core/tasks.py
+++ core/tasks.py
@@ -1,12 +1,24 @@
-from celery import shared_task
-from .parser import XMLParser
-from .models import Channel, Episode, XmlLink
-# from celery.utils.log import get_task_logger
+# from celery import shared_task
+# from .parser import XMLParser
+# from .models import Channel, Episode, XmlLink
+
+# # from celery.utils.log import get_task_logger
 
 
+# class RetryTask(Task):
+#     autoretry_for = (Exception,)
+#     retry_kwargs = {'max_retries': 5}
+#     retry_backoff = 2
 
-@shared_task
-def update_all_podcast():
-    url_list = Podcast.objects.all().values_list('podcast_url')
-    for url in url_list:
-        update_podcast.delay(url)
+
+# @shared_task
+# def update_all_podcast():
+#     url_list = Channel.objects.all().values_list('xml_link')
+#     for url in url_list:
+#         update_podcast.delay(url)
+#     return "URL sent for parse data in it."
+
+# @shared_task(bind=True, base=RetryTask)
+# def update_podcast(self, url):
+#     data = request.get(url).text
+#     parser = XMLParser()
