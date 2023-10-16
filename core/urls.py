82ca503058ed685e78e189c356c3e5df2f6262a1
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-16 15:57:00 +0330

    fix:comment update_episode path

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ExtractXMLChannel, ExtractXMLItems, UpdateEpisodesView
+from .views import ExtractXMLChannel, ExtractXMLItems
  
  
 urlpatterns = [
     path('extractchannel/', ExtractXMLChannel.as_view()),
     path('extractitem/', ExtractXMLItems.as_view()),
-    path('update_episode/', UpdateEpisodesView.as_view()),
+    # path('update_episode/', UpdateEpisodesView.as_view()),
     # path('test/', TestView.as_view()),
 ]
