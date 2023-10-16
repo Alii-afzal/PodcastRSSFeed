367f1ce0b04c2d3d389783d4773d416966f28651
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-16 15:57:26 +0330

    fix:comment UpdateEpisodesView class

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from .serializers import ChannelSerializer, EpisodeSerializer
 from rest_framework.response import Response
 from .models import Episode, Channel
-from .parser import xml_parser
+# from .parser import xml_parser
 
 class ExtractXMLChannel(APIView):
     def get(self, request):
@@ -19,18 +19,10 @@ class ExtractXMLItems(APIView):
         ser_data = EpisodeSerializer(instance=parsed_data, many=True)
         return Response(data=ser_data.data)
     
-class UpdateEpisodesView(APIView):
-    def get(self, request): 
-        print("update5")
-        xml_parser.update_episodes()  
-        print('update')
-        return Response({"message": "Episodes updated successfully"}, status=status.HTTP_200_OK)
+# class UpdateEpisodesView(APIView):
+#     def get(self, request): 
+#         print("update5")
+#         xml_parser.update_episodes()  
+#         print('update')
+#         return Response({"message": "Episodes updated successfully"}, status=status.HTTP_200_OK)
         
-# class TestView(APIView):
-#     def get(self, request):
-#         xml_parser = XMLParser()
-#         xml_parser.update_episodes()
-#         print("Reza")
-#         import sys
-#         sys.stdout.write("Farzam")
-#         return Response({"status":"Test"})
