d843e3fa4780537eee447548effcce6e1d935aee
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-16 15:54:54 +0330

    feat:add RetryTask class

--- core/tasks.py
+++ core/tasks.py
@@ -1,24 +1,14 @@
-# from celery import shared_task
-# from .parser import XMLParser
-# from .models import Channel, Episode, XmlLink
-
-# # from celery.utils.log import get_task_logger
-
-
-# class RetryTask(Task):
-#     autoretry_for = (Exception,)
-#     retry_kwargs = {'max_retries': 5}
-#     retry_backoff = 2
+import requests
+from celery import shared_task, Task
+from .parser import XMLParser
+from .models import Channel, Episode, XmlLink
 
 
-# @shared_task
-# def update_all_podcast():
-#     url_list = Channel.objects.all().values_list('xml_link')
-#     for url in url_list:
-#         update_podcast.delay(url)
-#     return "URL sent for parse data in it."
-
-# @shared_task(bind=True, base=RetryTask)
-# def update_podcast(self, url):
-#     data = request.get(url).text
-#     parser = XMLParser()
+class RetryTask(Task):
+    autoretry_for = (Exception,)
+    retry_kwargs = {'max_retries': 5}
+    retry_backoff = 2
+    retry_jitter = False
+    task_acks_late = True
+    worker_concurrency = 4
+    prefetch_multiplier = 1
