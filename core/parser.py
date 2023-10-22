ceea89dd9ab474fc197db138ac6ca791fba379e3
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-22 05:52:56 +0330

    chore:add channel_obj to item_parser for save in database

--- core/parser.py
+++ core/parser.py
@@ -4,15 +4,14 @@ import xml.etree.ElementTree as ET
 from .models import Channel, Episode, Category, XmlLink
 
 class XMLParser():
-    def __init__(self):
-        self.xml_link ='https://rss.art19.com/apology-line'
+    def __init__(self, xml_link):
+        self.xml_link = xml_link
         self.response = requests.get(self.xml_link)
         self.xml_data = self.response.text
         self.root = ET.fromstring(self.xml_data)
         self.itunes_namespace = {'itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd'}
         self.all_episodes = []
         self.channel_data = []
-        self.category_data = []
     
     def xml_link_parse(self):
         xml_link = self.xml_link
@@ -37,11 +36,6 @@ class XMLParser():
         xml_link = self.xml_link
         XmlLink.objects.create(xml_link=xml_link)
     
-    # def get_element_from_channel(self, tag):
-    #     for elm in self.root.findall(".//channel", self.itunes_namespace):
-    #         element = elm.find(tag).text if elm.find(tag) is not None else None
-    #     return element
-
     def channel_parser(self):
         for elm in self.root.findall(".//channel", self.itunes_namespace):
             title = elm.find('.//title').text if elm.find('.//title') is not None else None
@@ -54,43 +48,31 @@ class XMLParser():
             category = elm.find('.//itunes:category', self.itunes_namespace).get('text') if elm.find('.//itunes:category', self.itunes_namespace) is not None else None
             source =elm.find('.//link').text if elm.find('.//link') is not None else None
             owner = self.convert_text_to_boolianfield(elm.find('.//itunes:owner/itunes:name', self.itunes_namespace)) if elm.find('.//itunes:owner/itunes:name', self.itunes_namespace) is not None else None
-            # xml_link = self.xml_link
-    
-    
-        category = Category.objects.create(title=category)
-        
-        channel = {
-                'title' : title,
-                'subtitle' :subtitle,
-                'description' :description,
-                'pubDate' : pubDate,
-                'image' : image,
-                'language' : language,
-                'author' : author,
-                'category' : category,
-                'source' : source,
-                'owner' : owner,
-                # 'xml_link' : xml_link,   
-        }
-        self.channel_data.append(channel)
-        return self.channel_data, self.category_data
-        return channel
-    
-    def seve_channel_in_database(self):
-        for ch in self.channel_data:
-            Channel.objects.get_or_create(**ch)
-        # channel_instance = Channel()
-        # channel_instance.objects.get_or_create()
 
+        category,_ = Category.objects.get_or_create(title=category)
+        
     
-    # def save_category_in_database(self):
-    #     for category in self.category_data:
-    #         Category.objects.create(**category)
+        channel,_ = Channel.objects.get_or_create(
+                title = title,
+                subtitle = subtitle,
+                description = description,
+                pubDate = pubDate,
+                image = image,
+                language = language,
+                author = author,
+                category=category,
+                source = source,
+                owner = owner,
+                # xml_link=xml_link   
+        ) 
         
-    def item_parser(self):
-        # channel_parser = self.channel_parser()
-        # channel = Channel.objects.get(source=self.channel_data['source'])
+        return channel
+     
+    def seve_channel_in_database(self):
+        channel_data = self.channel_parser()
+        return channel_data
         
+    def item_parser(self, channel_obj):
         for elm in self.root.findall(".//item", self.itunes_namespace):
             title = elm.find('.//title').text if elm.find('.//title') is not None else None
             subtitle = elm.find('.//subtitle').text if elm.find('.//subtitle') is not None else None
@@ -101,9 +83,10 @@ class XMLParser():
             audio_file = elm.find('.//enclosure').attrib.get('url') if elm.find('.//enclosure') is not None else None
             guid = elm.find('.//guid').text if elm.find('.//guid') is not None else None
             explicit = self.convert_text_to_boolianfield(elm.find('.//itunes:explicit', self.itunes_namespace).text) if elm.find('.//itunes:explicit', self.itunes_namespace) is not None else None
+            
             episode_data = {
                 'title': title,
-                # 'channel': channel,
+                'channel': channel_obj,
                 'subtitle': subtitle,
                 'description': description,
                 'pubDate': pubDate,
@@ -125,13 +108,12 @@ class XMLParser():
     def update_episodes(self):
         new_episodes = self.item_parser()
 
-        existing_episodes = Episode.objects.filter()
+        existing_episodes = Episode.objects.all()
         existing_guids = set(existing.guid for existing in existing_episodes)
 
+        
         for episode in new_episodes:
             if episode['guid'] not in existing_guids:
                 print(episode['guid'])
                 Episode.objects.create(**episode)   
                 existing_guids.add(episode['guid'])
- 
-# xml_parser = XMLParser() 
