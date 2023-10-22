69f896a0a1d626640aa1a7b39061ab0ab027b1c5
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-22 05:54:17 +0330

    fix:delete try,except and comment some codes

--- core/management/commands/parse.py
+++ core/management/commands/parse.py
@@ -1,33 +1,32 @@
 from django.core.management.base import BaseCommand
 from core.parser import XMLParser
 import requests
+from core.tasks import update_podcast, update_all_podcasts
+
 
 class Command(BaseCommand):
     help = "Parse and update podcast data"
 
     def handle(self, *args, **options):
-        try:
-            xml_parser = XMLParser()
+        
+        xml_parser = XMLParser('https://rss.art19.com/apology-line')
 
-            xml_parser.item_parser()
-            xml_parser.save_items_in_database()
-            
-            xml_parser.channel_parser()
-            xml_parser.seve_channel_in_database()
+        # channel_data = xml_parser.seve_channel_in_database()
+        
+        # xml_parser.item_parser(channel_data)
+        
+        # xml_parser.save_items_in_database()
+        
 
-            xml_parser.xml_link_parse()
-            
-            # update_podcast.delay('https://rss.art19.com/apology-line')
-            # update_all_podcasts.delay()
+        # xml_parser.xml_link_parse()
+        
+        update_podcast.delay('https://rss.art19.com/apology-line')
+        update_all_podcasts.delay() 
                 
-            self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Successfully parsed and updated podcast data for all models)"
-                    )
-            )
-        except Exception as e:
-            self.stdout.write(
-                self.style.ERROR(
-                    f"Error while parsing and updating podcast rss data models \n {e})"
-                )
-            )
+            
+        # except Exception as e:
+        #     self.stdout.write(
+        #         self.style.ERROR(
+        #             f"Error while parsing and updating podcast rss data models \n {e})"
+        #         )
+        #     )
