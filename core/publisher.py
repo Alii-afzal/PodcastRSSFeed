6177b6661106648c486399c04cd297460bf45866
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-28 13:54:31 +0330

    feat:add login function to pulish class

--- core/publisher.py
+++ core/publisher.py
@@ -9,4 +9,12 @@ class Publish:
         result = self.channel.queue_declare(queue = '', exclusive = True)
         self.qname = result.method.queue
         
- 
+    def login(self, email, request_META):
+        notif = {
+            'email' : email,
+            'message' : f"{email} is login; {request_META}"
+        }
+        
+        self.response = None
+        self.channel.basic_publish(exchange='', routing_key = 'login', properties = pika.BasicProperties(delivery_mode=2), body = json.dumps(notif))
+        
