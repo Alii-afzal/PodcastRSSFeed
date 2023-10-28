ab91de5168001c9e79f10202c5a89bebeb45adf9
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-28 13:59:16 +0330

    fix:delete Type model

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Channel, Episode, News, Category, XmlLink, News, Type
+from .models import Channel, Episode, News, Category, XmlLink, News
 # Register your models here.
 admin.site.register(Channel)
 admin.site.register(Episode)
 admin.site.register(Category)
 admin.site.register(XmlLink)
 admin.site.register(News)
-admin.site.register(Type)
