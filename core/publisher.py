54dc9a9276ef2609fbbffb7d707baaddd1bb48d1
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-28 13:56:01 +0330

    feat:add update_podcast function to pulish class

--- core/publisher.py
+++ core/publisher.py
@@ -26,3 +26,10 @@ class Publish:
         self.response = None
         self.chanel.basic_publish(exchange = '', routing_key = 'register',properties = pika.BasicProperties(delivery_mode=2), body = json.dumps(notification))
         
+    def update_podcast(self, podcast):
+        notification={
+            'podcast' : podcast,
+            'message' : f"{podcast} has new episodes"
+        }
+        self.response = None
+        self.chanel.basic_publish(exchange = '', routing_key = 'update_podcast', properties = pika.BasicProperties(delivery_mode=2), body =notification)
