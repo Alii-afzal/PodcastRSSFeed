f10f1274df0d6e1d7f70d1944adb57603497c88c
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-29 23:36:51 +0330

    fix:fix update_podcast_callback function

--- accounts/consumer.py
+++ accounts/consumer.py
@@ -28,7 +28,7 @@ def login_consume():
     
     channel.start_consuming()
     
-def register_callback(chanel, method, properties, body):
+def register_callback(channel, method, properties, body):
     data = json.loads(body)
     user = User.objects.get(email=data['email'])
     notification_info = NotificationInfo.objects.create(message=data['message'])
@@ -36,33 +36,50 @@ def register_callback(chanel, method, properties, body):
 
 def register_consume():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBITMQ_HOST))
-    chanel = connection.channel()
+    channel = connection.channel()
 
-    chanel.queue_declare(queue='register')
-    chanel.basic_consume(queue='register', on_message_callback=login_callback)
+    channel.queue_declare(queue='register')
+    channel.basic_consume(queue='register', on_message_callback=login_callback)
 
-    chanel.start_consuming()
+    channel.start_consuming()
 
 
-def update_podcast_callback(chanel, method, properties, body):
-    data = json.loads(body)
+# def update_podcast_callback(channel, method, properties, body):
+#     data = json.loads(body)
     
-    channel_id = data['podcast']
+#     channel_id = data['podcast']
     
-    subscribers = Subscribe.objects.filter(channel=Channel.objects.get(id=channel_id))
-    if subscribers.exists():
-        for subscriber in subscribers:
-            notification = NotificationInfo.objects.create(message = data['message'])
-            user = User.objects.get(id=subscriber.user.id)
-            Notification.objects.create(user = user, message = notification)
+#     subscribers = Subscribe.objects.filter(channel=Channel.objects.get(id=channel_id))
+#     if subscribers.exists():
+#         for subscriber in subscribers:
+#             notification = NotificationInfo.objects.create(message = data['message'])
+#             user = User.objects.get(id=subscriber.user.id)
+#             Notification.objects.create(user = user, message = notification)
 
+
+def update_podcast_callback(channel, method, properties, body):
+    try:
+        data = json.loads(body)
+        channel_id = data['podcast']
+        # user = User.objects.get(email=data['email'])
+        subscribers = Subscribe.objects.filter(channel=Channel.objects.get(id=channel_id))
+        if subscribers.exists():
+            for subscriber in subscribers:
+                notification = NotificationInfo.objects.create(message=data['message'])
+                user = User.objects.get(id=subscriber.user.id)
+                print(user)
+                Notification.objects.create(user=user, message=notification)
+    except Exception as e:
+        print(e)
+    finally:
+        channel.basic_ack(delivery_tag=method.delivery_tag)
 
 def update_podcast_consume():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBITMQ_HOST))
-    chanel = connection.channel()
+    channel = connection.channel()
 
-    chanel.queue_declare(queue='update_podcast')
-    chanel.basic_consume(queue='update_podcast', on_message_callback=update_podcast_callback)
+    channel.queue_declare(queue='update_podcast')
+    channel.basic_consume(queue='update_podcast', on_message_callback=update_podcast_callback)
 
-    chanel.start_consuming()
+    channel.start_consuming()
     
