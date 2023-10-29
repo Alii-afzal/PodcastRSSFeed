0659dee57a090c784562a41ec5b57d0386ee5de0
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-29 10:09:22 +0330

    fix:remove some comments

--- interactions/models.py
+++ interactions/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
-from core.models import Channel, Episode
+from core.models import Channel, Episode, Category
 from accounts.models import User
 
 
@@ -11,22 +11,6 @@ class Subscribe(models.Model):
     
     def __str__(self):
         return f"{self.user} subscribe {self.channel}"
-    
-
-
-# class Subscribe(models.Model):
-#     user =  models.ForeignKey(User, on_delete=models.CASCADE)
-    
-#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-#     object_id = models.PositiveIntegerField()
-#     content_object = GenericForeignKey('content_type', 'object_id')
-    
-# class Like(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
-#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-#     object_id = models.PositiveIntegerField()
-#     content_object = GenericForeignKey('content_type', 'object_id')
 
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -34,8 +18,6 @@ class Like(models.Model):
     
     def __str__(self):
         return f"{self.user} liked {self.episode}"
-    
-    
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -45,27 +27,9 @@ class Comment(models.Model):
     def __str__(self):
         return f"{self.user} write comment for {self.episode}"
     
-    
-# class Comment(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     content = models.TextField(blank=False, null=False)
-    
-#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-#     object_id = models.PositiveIntegerField()
-#     content_object = GenericForeignKey('content_type', 'object_id')
-
-  
-
 class BookMark(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     episode = models.ForeignKey(Episode, on_delete=models.CASCADE)
     
     def __str__(self):
         return f"{self.episode} Bookmarked by {self.user}"
-  
-# class Bookmark(models.Model):
-#     user=models.ForeignKey(User, on_delete=models.CASCADE)
-    
-#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-#     object_id = models.PositiveIntegerField()
-#     content_object = GenericForeignKey('content_type', 'object_id')
