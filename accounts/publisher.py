e9f2386cf0cb156220f41d8c4bb406d99f650b1e
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-10-29 09:59:45 +0330

    chore:add routing_key to login,register,update_podcast

--- accounts/publisher.py
+++ accounts/publisher.py
@@ -14,17 +14,33 @@ class Publish:
     def login(self, email, request_META):
         notif = {
             'email' : email,
-            'message' : f"{email} is login; {request_META}"
+            'message' : f"{email} is login; {request_META}",
+            # 'user_agent': request_META,
+            'routing_key': 'login'
         }
         
         self.response = None
-        self.channel.basic_publish(exchange='', routing_key = 'login', properties = pika.BasicProperties(delivery_mode=2), body = json.dumps(notif))
+        self.channel.basic_publish(exchange='', routing_key = 'login',
+                                   properties = pika.BasicProperties(delivery_mode=2), body = json.dumps(notif))
         
         
     def register(self, email, request_META):
         notification={
             'email' : email,
-            'message' : f"{email} is register; {request_META}"
+            'message' : f"{email} is register; {request_META}",
+            'routing_key': 'register'
         }
         self.response = None
-        self.channel.basic_publish(exchange = '', routing_key = 'register',properties = pika.BasicProperties(delivery_mode=2), body = json.dumps(notification))
+        self.channel.basic_publish(exchange = '', routing_key = 'register',
+                                   properties = pika.BasicProperties(delivery_mode=2), body = json.dumps(notification))
+        
+        
+    def update_podcast(self, podcast):
+        notification={
+            'podcast' : podcast.id,
+            'message' : f"{podcast.title} has new episodes",
+            'routing_key': 'update_podcast'
+        }
+        self.response = None
+        self.channel.basic_publish(exchange = '', routing_key = 'update_podcast',
+                                    properties = pika.BasicProperties(delivery_mode=2), body = json.dumps(notification))
