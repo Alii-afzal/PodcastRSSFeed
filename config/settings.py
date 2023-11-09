a7b4744e6b0c313521184974d693e8e52ba94182
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-11-09 07:44:21 +0330

    chore:change LOGGING,Translation,CELERY_BEAT settings

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'interactions.apps.InteractionsConfig',
     'django_celery_beat',
+    'elasticsearch_dsl',
     'django_elasticsearch_dsl',
     'django_elasticsearch_dsl_drf',
 ]
@@ -60,12 +61,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'core.middleware.LogggingMiddleware'
+    
+    'config.middleware.CustomLoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -110,11 +113,12 @@ DATABASES = {
     }
 }
 
-from django_redis.cache import RedisCache
+# from django_redis.cache import RedisCache
+
 # CACHES = {
 #     "default": {
 #         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": "redis://redis:6379/1",
+#         "LOCATION": "redis://redis:6379/0",
 #         "OPTIONS": {
 #             "CLIENT_CLASS": "django_redis.client.DefaultClient",
 #         }
@@ -188,21 +192,17 @@ CORS_ALLOW_CREDENTIALS = True
 REDIS_HOST = env("REDIS_HOST")
 REDIS_PORT = env("REDIS_PORT")
 
-CELERY_BROKER_URL = env("CELERY_BROKER", default=f'redis://{REDIS_HOST}:{REDIS_PORT}')
-CELERY_RESULT_BACKEND = env("CELERY_BACKEND", default=f'redis://{REDIS_HOST}:{REDIS_PORT}')
+ELASTICSEARCH_HOST = env("ELASTICSEARCH_HOST")
+ELASTICSEARCH_PORT = env("ELASTICSEARCH_PORT")
 
-# CELERY_BEAT_SCHEDULE = {
-#     'update-podcast': {
-#         'task': 'core.tasks.update_all_podcasts',
-#         'schedule': crontab(minute=0, hour=4),
-#         # 'args': (16, 16),
-#     },
-# }
+CELERY_BROKER_URL = env("CELERY_BROKER", default=f'redis://{REDIS_HOST}:{REDIS_PORT}/1')
+CELERY_RESULT_BACKEND = env("CELERY_BACKEND", default=f'redis://{REDIS_HOST}:{REDIS_PORT}/1')
+
 
 CELERY_BEAT_SCHEDULE = {
     "sample_task": {
-        "task": "episode.tasks.update_all_podcast",
-        "schedule": crontab(minute="*/1"),
+        "task": "core.tasks.update_all_podcasts",
+        "schedule": crontab(minute='*/1'),
     },
 }
 
@@ -220,77 +220,35 @@ ELASTICSEARCH_DSL={
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    
+
     "handlers": {
-        "console": {
+        "elastic_handlers": {
             "level": "INFO",
-            # "filters": ["require_debug_true"],
-            "class": "logging.StreamHandler",
-            # "formatter": "simple",
-        },
-        "user_authenticate_file_handler":{
-            "level":"DEBUG",
-            "class":"logging.FileHandler",
-            "filename":"user_authenticate_logs.log",
-            "formatter":"main_formatter",
-        },
-        "celery_file_handler":{
-            "level":"DEBUG",
-            "class":"logging.FileHandler",
-            "filename":"celery_tasks_logs.log",
-            "formatter":"main_formatter",
-        },
-        "API_file_handler":{
-            "level":"DEBUG",
-            "class":"logging.FileHandler",
-            "filename":"API_endpoints_logs.log",
-            "formatter":"main_formatter",
-        },
-        
-        # "elasticsearch_handler": {
-        #     "level": "INFO",
-        #     "class": "django.utils.log.ElasticsearchHandler",
-        #     "filters": ["special"],
-        # },
-    },
-    
-    "formatters": {
-        "main_formatter": {
-            "format": "{levelname} {message}",
-            "style": "{",
+            "class": "config.elastic_log_handler.ElasticHandler",
         },
     },
-    
+
     "loggers": {
-        # "django": {
-        #     "handlers": ["console"],
-        #     "propagate": True,
-        # },
-        "user_authenticate_logger": {
-            "handlers": ["user_authenticate_file_handler",],
+        "elastic-logger": {
+            "handlers": ["elastic_handlers"],
             "level": "INFO",
-            "propagate": False,
-        },
-        "celery_tasks_logger": {
-            "handlers": ["celery_file_handler"],
-            "level": "INFO",
-            "propagate": False,
-        },
-        "API_endpoints_logger": {
-            "handlers": ["API_file_handler",],
-            "level": "INFO",
-            "propagate": False,
+            'propagate': True
         },
     },
-    
-    
-    # "filters": {
-    #     "special": {
-    #         "()": "project.logging.SpecialFilter",
-    #         "foo": "bar",
-    #     },
-    #     "require_debug_true": {
-    #         "()": "django.utils.log.RequireDebugTrue",
-    #     },
-    # },
 }
+
+
+from django.utils.translation import gettext_lazy as _
+
+LANGUAGE_CODE='en'
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('fa', _('Persian')),
+)
+
+LOCALE_PATH = (
+    os.path.join(BASE_DIR, 'locale/'),
+)
+
+AUTH_USER_MODEL = 'accounts.User'
