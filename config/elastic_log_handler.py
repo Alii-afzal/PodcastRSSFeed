6860be60094d5e64b6a4919226ca72022eae8e8b
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-11-09 14:58:32 +0330

    feat:add ElasticHandler function

--- config/elastic_log_handler.py
+++ config/elastic_log_handler.py
@@ -8,6 +8,18 @@ import pytz
 
 tz = pytz.timezone(settings.TIME_ZONE)
 
+class ElasticHandler(logging.Handler):
+
+    def __init__(self):
+        super().__init__()
+        self.es = Elasticsearch('http://elasticsearch:9200')
+        self.sender = LogSender(self.es)
+
+    def emit(self, record):
+        try:
+            self.sender.writeLog(record, formatter=self.format)
+        except Exception:
+            self.handleError(record)
 
 
 class LogSender:
