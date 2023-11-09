2d90f4539a905e9aa9a544f9731f1f843b0144c6
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-11-09 08:04:30 +0330

    feat:add XmlLinkSerializer function

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from .models import Channel, Episode, News,  XmlLink
 
+
+class XmlLinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = XmlLink
+        # fields = ('id', 'url')
+        fields = "__all__"
 class ChannelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Channel
