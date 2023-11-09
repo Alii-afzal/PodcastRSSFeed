3c7da9bf3f84333de0128a014c11e690cb579633
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-11-09 14:20:28 +0330

    feat:add extractepisode url path

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import ExtractXMLChannel, ExtractXMLItems
+from .views import ExtractXMLChannel, ExtractXMLItems, AddPodcastUrlView, UpdatePodcastTaskAPIView, UpdateEpisodesView, AddPodcastAPIView
  
  
 urlpatterns = [
     path('extractchannel/', ExtractXMLChannel.as_view()),
-    path('extractitem/', ExtractXMLItems.as_view()),
-    # path('update_episode/', UpdateEpisodesView.as_view()),
-    # path('test/', TestView.as_view()),
+    path('extractepisode/', ExtractXMLItems.as_view()),
 ]
+ 
