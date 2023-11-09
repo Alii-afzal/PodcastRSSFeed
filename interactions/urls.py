edcc1e37d964f901624a9a2dc23805c3d5653317
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-11-09 07:39:28 +0330

    feat:add delete-comment url

--- interactions/urls.py
+++ interactions/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 # from .views import like_episode
-from .views import LikeAPIView, CommentAPIView, SubscribeAPIView, BookMarkAPIView
+from .views import LikeAPIView, CommentAPIView, SubscribeAPIView, BookMarkAPIView, CommentDeleteAPIView
 
 urlpatterns = [
     path('like/', LikeAPIView.as_view()),
     path('comment/', CommentAPIView.as_view()),
     path('subscribe/', SubscribeAPIView.as_view()),
     path('bookmark/', BookMarkAPIView.as_view()),
+    path('delete-comment/<int:comment_id>/', CommentDeleteAPIView.as_view()),
 ]
