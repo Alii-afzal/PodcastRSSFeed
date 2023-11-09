8c43616b3d9daccad1a235e20b38389f4dd52fd9
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-11-09 14:46:03 +0330

    feat:add retry function for logging

--- core/tasks.py
+++ core/tasks.py
@@ -3,18 +3,27 @@ from celery import shared_task, Task
 from .parser import XMLParser
 from .models import Channel, Episode, XmlLink
 import logging
+from .task_logs import log_task_info
 
 
-logger = logging.getLogger(__name__)
 
 class RetryTask(Task):
     autoretry_for = (Exception,)
     retry_kwargs = {'max_retries': 5}
     retry_backoff = 2
-    retry_jitter = False
+    # retry_jitter = False
     task_acks_late = True
     worker_concurrency = 4
-    prefetch_multiplier = 1
+    # prefetch_multiplier = 1
+    
+    def retry(self, args=None, kwargs=None, exc=None, throw=True,
+              eta=None, countdown=None, max_retries=None, **options):
+        retry_count = self.request.retries
+        retry_eta = eta or (countdown and f'countdown={countdown}') or 'default'
+        log_task_info(self.name, 'warning', f'Retrying task {self.name} (retry {retry_count}) in {retry_eta} seconds',self.request.id, args, kwargs, exception=exc, retry_count=retry_count, max_retries=max_retries, retry_eta=retry_eta)
+
+        super().retry(args, kwargs, exc, throw, eta, countdown, max_retries, **options)
+
 
 
 @shared_task(bind=True, base=RetryTask)
@@ -22,16 +31,15 @@ def update_all_podcasts(self):
     xml_links = XmlLink.objects.all()
 
     for xml_link in xml_links:
-        print(xml_link)
         url = xml_link.xml_link
         update_podcast.delay(url)
 
-    return "URLs sent to parsing!"
+    # logger.error('Update aoo podcasts done successfully')
+    return "All episodes updated successfully."
 
 @shared_task(bind=True, base=RetryTask)
 def update_podcast(self, url):
-    data = requests.get(url).text
-    parser = XMLParser(xml_link=data)
+    parser = XMLParser(xml_link=url)
     parser.update_episodes()
 
     return 'Update podcast task is complete'
