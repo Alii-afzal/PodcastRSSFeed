93c3e067402c775e01fcb7dcfdf2e43a1e807b10
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-11-09 08:46:50 +0330

    fix:delete jwtauthenticate url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from accounts.views import RegisterAPIView, LoginAPIView, AuthenticatedView, RefreshAPIView, LogoutAPIView
+from accounts.views import RegisterAPIView, LoginAPIView, RefreshAPIView, LogoutAPIView
 # , UserAPIView, RefreshAPIView, LogoutAPIView
 
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
-    path('jwtauthenticate/', AuthenticatedView.as_view()),
     path('refresh/', RefreshAPIView.as_view(), name='refresh'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
 ]
