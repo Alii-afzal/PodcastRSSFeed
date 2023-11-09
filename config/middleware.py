52a449378ff4fdc7efb5a8cf718d7f31afc66e03
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-11-09 14:54:00 +0330

    feat: add log_format function

---
+++ config/middleware.py
@@ -0,0 +1,33 @@
+def log_format(request, response, exception=None):
+    user_id = request.user.id if request.user.is_authenticated else None
+    user_info ={
+        'user_id':user_id,
+        'user_email': request.user.email if request.user.is_authenticated else ' ',
+    }
+    remote_host = request.META.get("REMOTE_ADDR",'-')
+    request_line = request.method
+    
+    status_code = response.status_code if not exception else 500
+
+    response_size = response.get('Content-Length', ' ') if response else ""
+    print(response_size)
+    referrer = request.META.get('HTTP_REFERRER', '-')
+    elapsed_time = response.elapsed.total_seconds() if hasattr(response, 'elapsed') else None
+    user_agent = request.headers.get('user-agen')
+    event = f"{request.get_full_path()} HTTP/1.1"
+
+    message = str(exception) if exception else 'Request is successfully'
+    
+    return {
+        'user_info': user_info,
+        'remote_host': remote_host,
+        'request_line': request_line,
+        'status_code': status_code,
+        'response_size': response_size,
+        'referrer': referrer,
+        'elapsed_time': elapsed_time,
+        'message': message,
+        'user_agent': user_agent,
+        'event': event,
+    }
+
