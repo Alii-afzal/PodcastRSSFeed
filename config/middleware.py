05a63a9b14f0e8dc30aa5c55ae5107aac66d927a
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-11-09 14:55:54 +0330

    feat:add log_to_elasticsearch function

--- config/middleware.py
+++ config/middleware.py
@@ -53,3 +53,14 @@ def rss_log_format(body, exception=None):
         'message': message
     }
     
+import json
+import logging
+
+logger = logging.getLogger('elastic-logger')
+
+def log_to_elasticsearch(log_data, log_level):
+    if log_level == 'info':
+        logger.info(json.dumps(log_data))
+    elif log_level == 'error':
+        logger.error(json.dumps(log_data))
+        
