6bcf51e75db15728c864c617d71591cd1d5088b6
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-11-09 14:50:05 +0330

    chore:complete login_callback function

--- accounts/consumer.py
+++ accounts/consumer.py
@@ -1,3 +1,4 @@
+import logging
 import pika
 import json
 import time
@@ -11,14 +12,29 @@ from accounts.models import User, NotificationInfo, Notification
 from interactions.models import BookMark, Subscribe
 from config import settings
 
+from config.middleware import authentication_logs_format 
+
 from core.models import Channel
 
+
+# user_authenticate_logger = logging.getLogger('user_authenticate_logger')
+
+# celery_tasks_logger = logging.getLogger('celery_tasks_logger')
+
+logger = logging.getLogger('elastic-logger')
+
 def login_callback(ch, method, properties, body):
-    data = json.loads(body)
-    user = User.objects.get(email=data['email'])
-    notification_info = NotificationInfo.objects.create(message=data['message'])
-    Notification.objects.create(user=user, message=notification_info)
-    
+    try:
+        data = json.loads(body)
+        user = User.objects.get(email=data['email'])
+        notification_info = NotificationInfo.objects.create(message=data['message'])
+        Notification.objects.create(user=user, message=notification_info)
+        # user_authenticate_logger.info(body)
+        log_data = authentication_logs_format(user, data)
+        logger.info(json.dumps(log_data))
+    except Exception as error:
+        log_data = authentication_logs_format(user, data, error)
+        logger.error(json.dumps(log_data))
 def login_consume():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBITMQ_HOST))
     channel = connection.channel()
@@ -33,6 +49,8 @@ def register_callback(channel, method, properties, body):
     user = User.objects.get(email=data['email'])
     notification_info = NotificationInfo.objects.create(message=data['message'])
     Notification.objects.create(user = user, message = notification_info)
+    # user_authenticate_logger.info(body)
+    
 
 def register_consume():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBITMQ_HOST))
@@ -42,19 +60,6 @@ def register_consume():
     channel.basic_consume(queue='register', on_message_callback=login_callback)
 
     channel.start_consuming()
-
-
-# def update_podcast_callback(channel, method, properties, body):
-#     data = json.loads(body)
-    
-#     channel_id = data['podcast']
-    
-#     subscribers = Subscribe.objects.filter(channel=Channel.objects.get(id=channel_id))
-#     if subscribers.exists():
-#         for subscriber in subscribers:
-#             notification = NotificationInfo.objects.create(message = data['message'])
-#             user = User.objects.get(id=subscriber.user.id)
-#             Notification.objects.create(user = user, message = notification)
 
 
 def update_podcast_callback(channel, method, properties, body):
@@ -67,12 +72,13 @@ def update_podcast_callback(channel, method, properties, body):
             for subscriber in subscribers:
                 notification = NotificationInfo.objects.create(message=data['message'])
                 user = User.objects.get(id=subscriber.user.id)
-                print(user)
+                # print(user)
                 Notification.objects.create(user=user, message=notification)
     except Exception as e:
         print(e)
     finally:
         channel.basic_ack(delivery_tag=method.delivery_tag)
+    # celery_tasks_logger.info(body)
 
 def update_podcast_consume():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBITMQ_HOST))
