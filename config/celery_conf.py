690fdd4ab2de4db52fe0c68c4d1c7dfba1a14197
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-11-09 14:51:51 +0330

    fix:delete some comments

--- config/celery_conf.py
+++ config/celery_conf.py
@@ -6,5 +6,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 celery_app = Celery('config')
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
 celery_app.autodiscover_tasks()
-
-# CELERY_TIMEZONE = TOMEZONE
